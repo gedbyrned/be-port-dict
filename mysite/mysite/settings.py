c5bb6109af23c919878f3d7ca8b2f69e04732453
Gerard Byrne <gedbyrned@gmail.com>
2024-09-10 17:36:44 +0100

    add allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",['be-port-dict-1.onrender.com', 'localhost', '127.0.0.1']
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
